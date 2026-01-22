421557f4a39f654471c5e557c15b12241b37a8b0
kevinCV2 <cvkcastrovargas16@gmail.com>
2026-01-22 08:16:55 -0600

    1.2

--- src/sistemafarmacia/SistemaFarmacia.java
+++ src/sistemafarmacia/SistemaFarmacia.java
@@ -2,18 +2,43 @@ package sistemafarmacia;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import sistemafarmacia.ui.DashboardView;
 
 public class SistemaFarmacia extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        // ================= ICONO DE LA APP =================
+        Image icon = new Image(
+                SistemaFarmacia.class.getResourceAsStream(
+                        "/sistemafarmacia/assets/app_icon.jpg"
+                )
+        );
+        stage.getIcons().add(icon);
+
+        // ================= DASHBOARD =================
         DashboardView dashboard = new DashboardView();
         Scene scene = new Scene(dashboard.getRoot(), 1200, 720);
 
         stage.setTitle("Sistema de Gestión - Farmacia");
         stage.setScene(scene);
+
+        // ================= PANTALLA COMPLETA =================
+        stage.setFullScreenExitHint(""); // Quita el mensaje de ESC
+        stage.setFullScreen(true);       // Oculta barra de tareas
+
+        // ================= BLOQUEAR TECLA ESC =================
+        stage.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            if (e.getCode() == KeyCode.ESCAPE) {
+                e.consume(); // Bloquea ESC
+            }
+        });
+
         stage.show();
     }
 
